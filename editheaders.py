5bacaeac9e8c22cc65fa0859f7e4aefb46ae846f
Jo Taylor <jotaylor@stsci.edu>
2016-08-08 12:58:00 -0400

    Changed dir/file references to os.path.join and removed dir variable

--- editheaders.py
+++ editheaders.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 from stistools import calstis
 import calcos
@@ -17,13 +18,13 @@ subdirlist = ['smov4', 'cycle17', 'cycle18', 'cycle19', 'cycle20', 'cycle21', 'c
 reffiles = ['w5g1439sl_13.fits', 'w5g1439sl_33.fits', 'w5g1439sl_53.fits', 'w5g1439sl_17.fits', 'w5g1439sl_37.fits', 'w5g1439sl_57.fits', 'w5g1439sl_23.fits', 'w5g1439sl_43.fits', 'w5g1439s_l7.fits', 'w5g1439sl_27.fits', 'w5g1439sl_47.fits']
 
 for subdir in subdirlist:
-	dir = rootdir + subdir
-	rawfiles = glob.glob(dir + '/*_rawtag.fits')
+	mydir = os.path.join(rootdir, subdir)
+	rawfiles = glob.glob(os.path.join(mydir, '*_rawtag.fits'))
 	for ref in reffiles:
 		outputfolder = ref.split('_')[1].split('.')[0]
 		for file in rawfiles:
 			fits.setval(file, 'XTRACTAB', value = '/user/esnyder/nuvxtractabproj/new1dxfiles/' + str(ref), ext = 0)
 	
-		asnfiles = glob.glob(dir + '/*_asn.fits')
+		asnfiles = glob.glob(os.path.join(mydir, '*_asn.fits'))
 		for file in asnfiles:
-			calcos.calcos(file,outdir=dir+'/'+outputfolder+'/')
+			calcos.calcos(file,outdir=mydir+'/'+outputfolder+'/')
